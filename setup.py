9f186e25cc76b7bea0cfaef100c3ee4f081c086f
Gaurav <gauravpatni1923@gmail.com>
2025-04-20 12:10:59 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Gaurav',
+    author_email= 'gauravpatni1923@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
